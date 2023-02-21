d8a41a8caf681ef72aaeb314cd7bcb8f73f6ec6c
Felipe Torrezan <felipe.torrezan@iar.com>
2023-02-21 16:39:57 +0100

    Fix: missing demo.h

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "demo.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
